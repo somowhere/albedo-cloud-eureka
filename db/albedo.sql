4888490f7bfa0c9522ba143c6d7b61f30a9b049e
somowhere <somewhere0813@gmail.com>
2019-07-28 09:42:01 +0800

    update code

--- db/albedo.sql
+++ db/albedo.sql
@@ -5,7 +5,7 @@
  Source Server Type    : MySQL
  Source Server Version : 50725
  Source Host           : localhost:3306
- Source Schema         : albedo
+ Source Schema         : albedo-cloud
 
  Target Server Type    : MySQL
  Target Server Version : 50725
